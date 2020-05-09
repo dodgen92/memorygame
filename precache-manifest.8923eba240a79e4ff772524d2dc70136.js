5063e04b1349f3abcdf222b8c2aab25748260b73
Tyler Dodgen <tylerremingtondodgen@gmail.com>
2020-05-09 05:41:12 -0400

    Updates

---
+++ precache-manifest.8923eba240a79e4ff772524d2dc70136.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "360954aa7e42c6afa66123b1415e2d92",
+    "url": "/dodgen92/memorygame/index.html"
+  },
+  {
+    "revision": "d52e4316bb2e74460c26",
+    "url": "/dodgen92/memorygame/static/css/main.559ca08b.chunk.css"
+  },
+  {
+    "revision": "a7ee9c55de03050652a2",
+    "url": "/dodgen92/memorygame/static/js/2.8d5cc82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dodgen92/memorygame/static/js/2.8d5cc82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d52e4316bb2e74460c26",
+    "url": "/dodgen92/memorygame/static/js/main.0b3ef0b1.chunk.js"
+  },
+  {
+    "revision": "ea100976a4371ee13dd6",
+    "url": "/dodgen92/memorygame/static/js/runtime-main.8cce5c54.js"
+  }
+]);
